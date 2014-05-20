7b57a156414b39592d065379b6c90277eb1de433
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-05-20 12:04:26 +0800

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,38 @@
+/**
+ * 2014-05-19 23:22
+ * Phoenix Nemo <i at phoenixlzx dot com>
+ * License MIT | http://opensource.org/licenses/MIT
+ */
+
+function init() {
+
+    chrome.topSites.get(function(topSitesArray) {
+
+        var sites = [];
+
+        for (var i = 0; i <= 7; i++) {
+            sites.push('<li><a href="' + topSitesArray[i].url + '" class="top">' + topSitesArray[i].title + '</a></li>');
+        }
+
+        $('ul#topSites').append(sites);
+
+        // set background
+        var images = [
+            '001.jpg',
+            '002.jpg',
+            '003.jpg',
+            '004.jpg',
+            '005.jpg',
+            '006.jpg',
+            '007.jpg',
+            '008.jpg',
+            '009.jpg',
+            '010.jpg'
+        ];
+        $('body').css({'background-image': 'url(/img/bg/' + images[Math.floor(Math.random() * images.length)] + ')' });
+
+    });
+
+}
+
+init();
